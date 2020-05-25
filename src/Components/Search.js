bdaba1fe7842f6730b6f988f5078c592b4b373f9
Vidushi Dwivedi <37553084+Vidz51@users.noreply.github.com>
2020-05-25 23:05:21 +0530

    Add files via upload

---
+++ src/Components/Search.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+ 
+    state = { artistQuery: ""}
+    updateArtistQuery = () => {
+        this.setState({ artistQuery: event.target.value })
+    }
+    handleKeyPress = event => {
+        if (event.key === "Enter") {
+            this.searchArtist();
+        }
+    }
+
+    searchArtist = () => {
+        this.props.searchArtist(this.state.artistQuery);
+    }
+    render() {
+        return (
+            <div>
+                <input
+                    onChange={this.updateArtistQuery}
+                    placeholder="Search for an Artist"
+                    onKeyPress={this.handleKeyPress} />
+                <button onClick={this.searchArtist}> Search</button>
+            </div>
+        )
+    }
+}
+
+export default Search;
